5bd3cf5dd73034d83cbe3ac95b84cff36f3b6885
ksenia-nesytova <passedbyabyss@gmail.com>
2024-02-04 13:13:31 +0700

    add: get-request for actors

--- server/server.js
+++ server/server.js
@@ -32,6 +32,26 @@ app.use((req, res, next) => {
    }
  });
 
+app.get('/actors', async (req, res) => {
+  try {
+    const actors = await pool.query(`
+      select entities_id, entities.name AS name, entities.description as description, 
+      entities.start_day as start_day, 
+      entities.start_month as start_month, 
+      entities.start_year as start_year,
+      entities.end_day as end_day, 
+      entities.end_month  as end_month , 
+      entities.end_year  as end_year  from actors
+      left join entities on actors.entities_id = entities.id;
+
+    `);
+    console.log(actors, actors.rows)
+    res.json(actors.rows);
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 
 app.listen(3000, () => {
