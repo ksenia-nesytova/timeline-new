4269082fcbb8fd5183a577582ae4616f4d88f8b9
ksenia-nesytova <passedbyabyss@gmail.com>
2024-02-04 15:40:05 +0700

    add: date percision flags

--- src/app/filter-panel/filter-panel.component.ts
+++ src/app/filter-panel/filter-panel.component.ts
@@ -8,8 +8,13 @@ import { ApiService } from '../api.service';
 })
 
 export class FilterPanelComponent {
-  public impreciseDate: boolean = false;
-  public newEntryData: { name: string, start: string, type: string } = { name: '', start: '0001-01-01T00:00', type: '' };
+  public isDateImprecise: boolean = false;
+  public isYearImprecise: boolean = false;
+  public isMonthImprecise: boolean = false;
+  public isDayImprecise: boolean = false;
+
+
+  public newEntryData: { name: string, start_date: string, end_date: string, date_precision: number, type: string } = { name: '', start_date: '0001-01-01T00:00', end_date: '', date_precision: 0, type: '' };
   private _entityType: string = this.newEntryData.type;
 
   constructor(private _apiService: ApiService) { };
