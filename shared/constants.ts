e7d9419c53025cbb6c9bed4721fb35f534bbfaac
ksenia-nesytova <passedbyabyss@gmail.com>
2024-02-24 13:56:27 +0700

    add: consts file

---
+++ shared/constants.ts
@@ -0,0 +1,8 @@
+export const enum EntityTypes {
+    ITEM = 'item',
+    ACTOR = 'actor',
+    EVENT = 'event',
+    INSTITUTION = 'institution',
+    LOCATION = 'location',
+    PERIOD = 'period'
+}
