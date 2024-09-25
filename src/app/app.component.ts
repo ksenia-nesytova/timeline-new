7642a6969a57f8c00c8345d0168c1237fcc9fe01
ksenia-nesytova <passedbyabyss@gmail.com>
2024-09-25 23:08:32 +0700

    add: better test data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,36 +21,51 @@ export class AppComponent implements OnInit {
   constructor() { };
 
   ngOnInit() {
-      // Declare the chart dimensions and margins.
-      const width = 640;
-      const height = 400;
-      // const marginTop = 20;
-      const marginRight = 20;
-      const marginBottom = 30;
-      const marginLeft = 70;
+    // Declare the chart dimensions and margins.
+    const width = 640;
+    const height = 400;
+    // const marginTop = 20;
+    const marginRight = 20;
+    const marginBottom = 30;
+    const marginLeft = 70;
 
-//https://d3js.org/d3-axis
+    //https://d3js.org/d3-axis
 
-      const line = {
-        title: "The Revolution Era",
-        events: [
-          {
-            title: "Revolution",
-            year: 1917,
-          },
-          {
-            title: "The clergy is radicalized",
-            year: 1919,
-          },
-          {
-            title: "Mummification of the Glorious Leader",
-            year: 1937,
-          },
-        ],
-      };
-    
-      // Declare the x (horizontal position) scale.
-      const x = d3.scaleUtc()
+    const line = {
+      title: "That one time something happened",
+      events: [
+        {
+          "id": 37,
+          "name": "Julius Caesar",
+          "description": 'some guy',
+          "start_date": -65306304000,
+          "end_date": -63549360000,
+          "start_date_precision": null,
+          "end_date_precision": null
+        },
+        {
+          "id": 38,
+          "name": "Julius Caesar Birth",
+          "description": 'born',
+          "start_date": -65306304000,
+          "end_date": -63549360000,
+          "start_date_precision": null,
+          "end_date_precision": null
+        },
+        {
+          "id": 39,
+          "name": "Assassination of Julius Caesar",
+          "description": null,
+          "start_date": -63549360000,
+          "end_date": -63549360000,
+          "start_date_precision": null,
+          "end_date_precision": null
+        }
+      ],
+    };
+
+    // Declare the x (horizontal position) scale.
+    const x = d3.scaleUtc()
       .domain([new Date(line.events[0].start_date), new Date(line.events[line.events.length - 1].end_date)])
       .range([marginLeft, width - marginRight])
       .nice();
@@ -59,21 +74,21 @@ export class AppComponent implements OnInit {
     console.log(new Date(line.events[2].end_date))
     const xAxis = d3.axisBottom(x);
     xAxis.ticks(line.events.length);
-    
-      // Create the SVG container.
-      const svg = d3.select("#timeline-container")
-      .append("svg")
-          .attr("width", width)
-          .attr("height", height);
 
-      // Add the x-axis.
-      svg.append("g")
-          .attr("transform", `translate(0,${height - marginBottom})`)
+    // Create the SVG container.
+    const svg = d3.select("#timeline-container")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    // Add the x-axis.
+    svg.append("g")
+      .attr("transform", `translate(0,${height - marginBottom})`)
       .call(xAxis);
 
-      // Return the SVG element.
-      return svg.node();
-    }
+    // Return the SVG element.
+    return svg.node();
+  }
 
-  
+
 }
